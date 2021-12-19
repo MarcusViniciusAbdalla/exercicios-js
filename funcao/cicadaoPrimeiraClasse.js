58b99a444ce14988f86aad46e58c81e2395246a6
MarcusViniciusAbdalla <marcusviniciusabdalla@gmail.com>
2021-12-18 22:57:21 -0300

    Executando a função

--- funcao/cicadaoPrimeiraClasse.js
+++ funcao/cicadaoPrimeiraClasse.js
@@ -1,5 +1,7 @@
-function qualquer(){}
+function qualquer(){};
 
-const qualquer1 = function (){}
+const qualquer1 = function (){};
 
-const array = [function(c,d) {return c + d }, qualquer , qualquer1]
+const array = [function(c,d) {return c + d }, qualquer , qualquer1];
+console.log(array[0](10,10));
+
