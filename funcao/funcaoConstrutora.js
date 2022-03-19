261a6563a567d0b5be03363be38b87ab1820ad6f
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-03-18 21:35:11 -0300

    Update funcaoConstrutora.js

--- funcao/funcaoConstrutora.js
+++ funcao/funcaoConstrutora.js
@@ -25,3 +25,6 @@ console.log(uno.getVelocidadeAtual())
 const ferrari = new Carro(350, 20)
 ferrari.acelerar()
 console.log(ferrari.getVelocidadeAtual()) 
+
+console.log(typeof Carro)
+console.log(typeof ferrari)
