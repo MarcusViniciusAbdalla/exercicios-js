9d6764109f9d798c7c1c8ade7619c89a5e552760
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-03-19 14:55:17 -0300

    Create IIFE.js

---
+++ funcao/IIFE.js
@@ -0,0 +1,8 @@
+// Immediately Invoked Function Expression
+
+(function(){
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()
+
+//Muito usado para fugir do escopo global
