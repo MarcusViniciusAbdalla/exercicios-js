01f57d38865eb0bd3bcdc22f0a81f1dad35097bd
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-03-18 23:18:34 -0300

    Create factory2.js

---
+++ funcao/factory2.js
@@ -0,0 +1,11 @@
+function criarProduto(nome , preco){
+    return {
+        nome,
+        preco,
+        desconto:0.1 
+    }
+}
+
+console.log(criarProduto('Notebook', 2199.49))
+console.log(criarProduto('Ipad', 3199.49))
+console.log(criarProduto('Celular', 1199.49))
