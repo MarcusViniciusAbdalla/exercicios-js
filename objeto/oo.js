e12ec521734fe4de20d163a3f4dfa255d86e2c44
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-03-20 18:40:10 -0300

    Create oo.js

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// Código não executável!!!
+
+//procedural
+processamento(valor1, valor2, valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento()  //Foco passou a ser o objeto
+
+// Pricípios Importantes
+// 1. abstração
+// 2. encapsulamento
+// 3. herança(prototype)
+// 4. polimorfismo
