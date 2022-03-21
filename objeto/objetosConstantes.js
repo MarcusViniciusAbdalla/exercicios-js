7cc47d076fc59331108d6fdf6af606626d43409c
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-03-20 22:25:35 -0300

    Create objetosConstantes.js

---
+++ objeto/objetosConstantes.js
@@ -0,0 +1,16 @@
+// pessoa -> 123 -> {...}
+const pessoa = {nome: 'João'}
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+// pessoa <- 456 -> {...}
+// pessoa = {nome: 'Ana'}
+
+Object.freeze(pessoa)
+pessoa.nome = 'Maria'
+console.log(pessoa.nome)
+
+const pessoaConstante = Object.freeze({nome:'Junior'})
+console.log(pessoaConstante);
+
+
