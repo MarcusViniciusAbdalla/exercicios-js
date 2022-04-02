9dbd11688fab49fe3fe1642ed0aeb01aa6d14932
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-04-01 21:40:53 -0300

    Update jsonVsObj.js

--- objeto/jsonVsObj.js
+++ objeto/jsonVsObj.js
@@ -6,5 +6,5 @@ console.log(JSON.stringify(obj))
 //console.log(JSON.parse("{'a': 1, 'b': 2, 'c': 3}"))
 
 console.log(JSON.parse('{"a": 1, "b": 2, "c": 3}'))
-console.log(JSON.parse('{"a": 1, "b": "string", "c": true, "d": {}, "e": [] }'))
+console.log(JSON.parse('{"a": 1.7, "b": "string", "c": true, "d": {}, "e": [] }'))
 
