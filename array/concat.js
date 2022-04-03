0a6d586f728a2f69e1b076e6b2bc1db80c7395a2
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-04-03 16:54:08 -0300

    Create concat.js

---
+++ array/concat.js
@@ -0,0 +1,7 @@
+const daughters = [ 'Ualeskah' , 'Cibalena']
+const sons = ['Uoxiton' , 'Uesclei']
+
+const everyone = daughters.concat(sons)
+console.log(everyone,daughters,sons)
+
+console.log([].concat([1 , 2 ] , [3 ,4] ,5 , [[6 , 7 ]]))
