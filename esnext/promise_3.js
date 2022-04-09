71283a29884333f55428293e3b7b9b1209dd917a
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-04-09 19:21:40 -0300

    Update promise_3.js

--- esnext/promise_3.js
+++ esnext/promise_3.js
@@ -13,4 +13,4 @@ function gerarNumerosEntre(min , max) {
 gerarNumerosEntre(1 , 60) 
     .then (num => num *10)
     .then (numX10 => `O número gerado foi ${numX10}`)
-    .then (console.log)
+    .then (console)
