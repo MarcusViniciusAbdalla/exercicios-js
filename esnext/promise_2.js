5fe48a09b1148e1763a22802071b403b545d4d9b
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-04-09 19:02:46 -0300

    Create promise_2.js

---
+++ esnext/promise_2.js
@@ -0,0 +1,38 @@
+
+setTimeout(function(){
+    console.log('Executando callback...')
+
+    setTimeout(function(){ 
+        console.log('Executando callback...')
+
+        setTimeout(function(){ 
+            console.log('Executando callback...')
+        }, 2000)    
+    }, 2000)
+}, 2000)
+
+
+function esperarPor(tempo = 2000) {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            console.log('Executando promise...')
+            resolve('Vishhhhhhhh')
+        }, tempo)
+    })
+}
+
+esperarPor(3000).then(texto => console.log(texto))
+
+function esperarPor(tempo = 2000) {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            console.log('Executando promise...')
+            resolve()
+        }, tempo)
+    })
+}
+
+esperarPor()
+    .then(() => esperarPor())
+    .then(esperarPor)
+    
