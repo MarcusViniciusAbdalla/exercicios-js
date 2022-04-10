afbe49d1e0213ee03e8a6fd665b3ed4945baadb6
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-04-09 22:38:40 -0300

    Update promise_3.js

--- esnext/promise_3.js
+++ esnext/promise_3.js
@@ -1,8 +1,5 @@
 function gerarNumerosEntre(min , max) {
-    if (min > max) {
-        [max , min] = [ min , max]        
-    }
-
+    if (min > max) [max , min] = [ min , max]   
     return new Promise (resolve => {
         const fator = max - min +1
         const aleatorio = parseInt(Math.random() * fator) + min
